19a7c474582ca941ce3e310eaeec693330ec6709
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-11-06 07:03:45 +0900

    Add weather API

--- App.js
+++ App.js
@@ -2,11 +2,22 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "4b4ff6b08388df14010977362fadd67d";
 
 export default class extends React.Component {
   state = {
     isLoding: true
   };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
+
   getLocation = async () => {
     try {
       const response = await Location.requestPermissionsAsync();
@@ -15,7 +26,9 @@ export default class extends React.Component {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
       // API로 위도,경도 보내고 날씨 정보 얻기
+      this.getWeather(latitude, longitude);
       console.log(latitude, longitude);
+      this.state({ isLoding: false });
     } catch (error) {
       Alert.alert("위치를 찾을 수 없습니다");
     }
