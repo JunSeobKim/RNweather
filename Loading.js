ddc9d0a2f7371e73ff19e94441e679dba9ad085d
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-11-12 23:59:39 +0900

    icon

--- Loading.js
+++ Loading.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function Loading() {
   return (
     <View style={Styles.container}>
+      <StatusBar barStyle="dark-content" />
       <Text style={Styles.text}>Weather</Text>
     </View>
   );
--- Weather.js
+++ Weather.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
-export default function Weather({ temp }) {
+const weatherOptions = {
+  Clouds: { iconName: "ios-cloud-outline", gradient: ["#bdc3c7", "#2c3e50"] },
+  Clear: { iconName: "ios-moon", gradient: ["#CAC531", "#F3F9A7"] }
+};
+
+export default function Weather({ temp, condition }) {
   return (
-    <View style={styles.container}>
-      <Text>{temp}</Text>
-    </View>
+    <LinearGradient colors={["#4c669f", "#ffffff"]} style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.halfContainer}>
+        <Ionicons
+          name={weatherOptions["Clear"].iconName}
+          size={96}
+          color="white"
+        />
+        <Text style={styles.temp}>{temp}º</Text>
+      </View>
+      <View style={styles.halfContainer}></View>
+    </LinearGradient>
   );
 }
 
@@ -29,6 +45,15 @@ Weather.propTypes = {
 // 스타일
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  temp: {
+    fontSize: 42,
+    color: "white"
+  },
+  halfContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
